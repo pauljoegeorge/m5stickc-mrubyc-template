c71e04db41ae8ed1c5a9e4599f3ee5d7cc831b15
paul-ml <pauljoe.george@monstar-lab.com>
2020-06-26 20:23:07 +0900

    chage for button A also

--- mrblib/loops/master.rb
+++ mrblib/loops/master.rb
@@ -3,8 +3,8 @@ while true
   # puts greet.hola
   if button_a_pressed?
     puts "Button A pressed"
-    puts_color(0, 30, 160, 100, "RED");
-    puts_screen(60, 60, "BUTTON A");
+    put_color(0, 30, 160, 100, "RED");
+    put_string(60, 60, "BUTTON A");
   end
   if button_b_pressed?
     puts "Button B pressed"
