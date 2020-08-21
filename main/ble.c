a9d9b7639ceee7cac55667a048fa315af7fda1b1
paul-ml <pauljoe.george@monstar-lab.com>
2020-08-21 19:38:53 +0900

    removing unwanted

--- main/ble.c
+++ main/ble.c
@@ -188,37 +188,12 @@ static const esp_gatts_attr_db_t gatt_db[HRS_IDX_NB] =
     /* Characteristic Declaration */
     [IDX_CHAR_A]     =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
-      CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read_write_notify}},
+      CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read}},
 
     /* Characteristic Value */
     [IDX_CHAR_VAL_A] =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_A, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
       GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
-
-    // /* Client Characteristic Configuration Descriptor */
-    // [IDX_CHAR_CFG_A]  =
-    // {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_client_config_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-    //   sizeof(uint16_t), sizeof(heart_measurement_ccc), (uint8_t *)heart_measurement_ccc}},
-
-    /* Characteristic Declaration */
-    [IDX_CHAR_B]      =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
-      CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read_write_notify}},
-
-    /* Characteristic Value */
-    [IDX_CHAR_VAL_B]  =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_B, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-      GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
-
-    // /* Characteristic Declaration */
-    // [IDX_CHAR_C]      =
-    // {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
-    //   CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_write}},
-
-    // /* Characteristic Value */
-    // [IDX_CHAR_VAL_C]  =
-    // {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_C, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-    //   GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
 
 };
 
@@ -563,8 +538,6 @@ void c_chime_rang(mrb_vm *vm, mrb_value *v, int argc){
     if(ble_paired == 1){
         if(bell_pushed == 1){
             bell_pushed = 0;
-            printf("%d\n", heart_rate_profile_tab[PROFILE_APP_IDX].remote_bda);
-            esp_ble_gap_disconnect(heart_rate_profile_tab[PROFILE_APP_IDX].remote_bda);
             SET_TRUE_RETURN();
             return; 
         }
@@ -579,65 +552,3 @@ void c_pairing_status(mrb_vm *vm, mrb_value *v, int argc){
     }
     SET_FALSE_RETURN();
 }
-// void app_main(void)
-// {
-//     esp_err_t ret;
-
-//     /* Initialize NVS. */
-//     ret = nvs_flash_init();
-//     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//         ESP_ERROR_CHECK(nvs_flash_erase());
-//         ret = nvs_flash_init();
-//     }
-//     ESP_ERROR_CHECK( ret );
-
-//     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-
-//     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-//     ret = esp_bt_controller_init(&bt_cfg);
-//     if (ret) {
-//         ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-//     if (ret) {
-//         ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bluedroid_init();
-//     if (ret) {
-//         ESP_LOGE(GATTS_TABLE_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bluedroid_enable();
-//     if (ret) {
-//         ESP_LOGE(GATTS_TABLE_TAG, "%s enable bluetooth failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_ble_gatts_register_callback(gatts_event_handler);
-//     if (ret){
-//         ESP_LOGE(GATTS_TABLE_TAG, "gatts register error, error code = %x", ret);
-//         return;
-//     }
-
-//     ret = esp_ble_gap_register_callback(gap_event_handler);
-//     if (ret){
-//         ESP_LOGE(GATTS_TABLE_TAG, "gap register error, error code = %x", ret);
-//         return;
-//     }
-
-//     ret = esp_ble_gatts_app_register(ESP_APP_ID);
-//     if (ret){
-//         ESP_LOGE(GATTS_TABLE_TAG, "gatts app register error, error code = %x", ret);
-//         return;
-//     }
-
-//     esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
-//     if (local_mtu_ret){
-//         ESP_LOGE(GATTS_TABLE_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
-//     }
-// }
