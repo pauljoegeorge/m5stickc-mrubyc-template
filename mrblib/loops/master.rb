8dead7f030209bc2c695a55e1ae729ba0fd1499f
paul-ml <pauljoe.george@monstar-lab.com>
2020-08-21 18:43:18 +0900

    fix ble

--- mrblib/loops/master.rb
+++ mrblib/loops/master.rb
@@ -1,4 +1,5 @@
 greet = Greet.new
+ble = Ble.new
 while true
   # puts greet.hola
   if button_a_pressed?
@@ -11,9 +12,11 @@ while true
     put_color(0, 30, 160, 100, "GREEN");
     put_string(60, 60, "BUTTON B");
   end
-  if(chime_rang?)
-    put_color(0, 30, 160, 100, "BLUE");
-    put_string(60, 60, "CHIME !!");
+  if ble.paired?
+    if  ble.chime_rang?
+      put_color(0, 30, 160, 100, "BLUE")
+      put_string(60, 60, "CHIME !!")
+    end
   end
   sleep 0.3
 end
--- mrblib/models/ble.rb
+++ mrblib/models/ble.rb
@@ -5,4 +5,12 @@ class Ble
 			ble_start_advertising
 		end
 	end
+
+	def paired?
+		ble_paired?
+	end
+
+	def chime_rang?
+		ble_chime_rang?
+	end
 end
