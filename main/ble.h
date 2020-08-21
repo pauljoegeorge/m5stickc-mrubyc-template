f8edcf6b37f1624d64e6268c7f67ce482ba5ca03
paul-ml <pauljoe.george@monstar-lab.com>
2020-08-21 18:51:32 +0900

    adding mrubyc methods to headers

--- main/ble.h
+++ main/ble.h
@@ -1,4 +1,5 @@
 #include "mrubyc.h"
 void c_ble_start_advertising(mrb_vm *vm, mrb_value *v, int argc);
 void c_ble_initialize(mrb_vm *vm, mrb_value *v, int argc);
-void c_ble_paired(mrb_vm *vm, mrb_value *v, int argc);
+void c_chime_rang(mrb_vm *vm, mrb_value *v, int argc);
+void c_pairing_status(mrb_vm *vm, mrb_value *v, int argc);
